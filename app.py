7a7a7e12812088c00beda36f1f4819b4cb76a8de
adiraju-rohit <adiraju.rohit@gmail.com>
2026-06-21 22:31:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-import modl, os
+import modl
+import os
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'nextop')
